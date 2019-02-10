9eaed6ed43b6ee11701f6a6aa925dcc4501a911c
kushthaker <kushthaker@gmail.com>
2019-02-09 23:24:27 -0500

    added case 2 scatter plot

--- lab1/functions/MAP.m
+++ lab1/functions/MAP.m
@@ -5,7 +5,7 @@
 % Callum Mitchell - 20608435
 
 function [dist] = MAP(n_1, n_2, mean_1, mean_2, covar_1, covar_2, X, Y)
-    dist = zeros(size(X)); % distance matrix of size meshgrid(x,y)
+    dist = zeros(size(X));
     Q0 = inv(covar_1) - inv(covar_2);
     Q1 = 2*(mean_2*inv(covar_2) - mean_1*inv(covar_1));
     Q2 = mean_1*inv(covar_1)*mean_1' - mean_2*inv(covar_2)*mean_2';
--- lab1/main.m
+++ lab1/main.m
@@ -15,6 +15,9 @@ class_sample_data;
 
 samples_a = getBivariateData(n_a, mean_a, covar_a);
 samples_b = getBivariateData(n_b, mean_b, covar_b);
+samples_c = getBivariateData(n_c, mean_c, covar_c);
+samples_d = getBivariateData(n_d, mean_d, covar_d);
+samples_e = getBivariateData(n_e, mean_e, covar_e);
 
 %% Compute Classifiers
 
@@ -49,5 +52,20 @@ scatter_b = scatter(samples_b(:,1), samples_b(:,2), 'rx');
 % Plot means
 plot(mean_a(1), mean_a(2), 'ks', 'MarkerSize',10, 'MarkerFaceColor',[0 0 0]);
 plot(mean_b(1), mean_b(2), 'ks', 'MarkerSize',10, 'MarkerFaceColor',[0 0 0]);
+
+hold off;
+
+figure(2);
+hold on; 
+
+% Plot sample data
+scatter_c = scatter(samples_c(:,1), samples_c(:,2), 'b^', 'filled');
+scatter_d = scatter(samples_d(:,1), samples_d(:,2), 'rx');
+scatter_e = scatter(samples_e(:,1), samples_e(:,2), 'go', 'filled');
+
+% Plot means
+plot(mean_c(1), mean_c(2), 'ks', 'MarkerSize',10, 'MarkerFaceColor',[0 0 0]);
+plot(mean_d(1), mean_d(2), 'ks', 'MarkerSize',10, 'MarkerFaceColor',[0 0 0]);
+plot(mean_e(1), mean_e(2), 'ks', 'MarkerSize',10, 'MarkerFaceColor',[0 0 0]);
 
 hold off;
