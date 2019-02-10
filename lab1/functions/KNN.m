2e2e0cb26f57fb52d3713d5770a7a990a85f5b94
kushthaker <kushthaker@gmail.com>
2019-02-10 02:27:29 -0500

    deleted get_MED and MED .asv autosaved files

--- lab1/functions/KNN.m
+++ lab1/functions/KNN.m
@@ -3,8 +3,6 @@
 %
 % Kush Thaker - 20517901
 % Callum Mitchell - 20608435
-%
-% Febuary 12 2017
 
 function [dist] = KNN(samples_a, samples_b, X, Y, K)
     dist = KNearestNeighbour(samples_a, X, Y, K) - KNearestNeighbour(samples_b, X, Y, K);
