e31a4f835a6fc9a597ee8f679626abbf2303dbc6
Callum <mitccall@gmail.com>
2019-02-12 18:59:59 -0500

    Use 2nd data set for nearest-neighbour error analysis

--- lab1/scripts/get_error_NN.m
+++ lab1/scripts/get_error_NN.m
@@ -3,8 +3,8 @@
 % 
 % Kush Thaker - 20517901
 % Callum Mitchell - 20608435
-[A_ClassifyA, A_ClassifyB] = get_error(n_a, KNN(samples_a, samples_b, samples_a(:,1), samples_a(:,2), 1), @(d) d < 0);
-[B_ClassifyB, B_ClassifyA] = get_error(n_b, KNN(samples_a, samples_b, samples_b(:,1), samples_b(:,2), 1), @(d) d > 0);
+[A_ClassifyA, A_ClassifyB] = get_error(n_a, KNN(samples_a, samples_b, samples_a_2nd(:,1), samples_a_2nd(:,2), 1), @(d) d < 0);
+[B_ClassifyB, B_ClassifyA] = get_error(n_b, KNN(samples_a, samples_b, samples_b_2nd(:,1), samples_b_2nd(:,2), 1), @(d) d > 0);
 
 NN_conf_matrix_ab = [
     [A_ClassifyA, B_ClassifyA];
@@ -16,9 +16,9 @@ disp(NN_conf_matrix_ab);
 
 P_error_NN_case_1 = (B_ClassifyA + A_ClassifyB)/(n_a + n_b)
 
-[C_ClassifyC, C_ClassifyD, C_ClassifyE] = get_error_cde(n_c, KNN(samples_c, samples_d, samples_c(:,1), samples_c(:,2), 1), KNN(samples_c, samples_e, samples_c(:,1), samples_c(:,2), 1), KNN(samples_d, samples_e, samples_c(:,1), samples_c(:,2), 1), @(d) d < 0);
-[D_ClassifyC, D_ClassifyD, D_ClassifyE] = get_error_cde(n_c, KNN(samples_c, samples_d, samples_d(:,1), samples_d(:,2), 1), KNN(samples_c, samples_e, samples_d(:,1), samples_d(:,2), 1), KNN(samples_d, samples_e, samples_d(:,1), samples_d(:,2), 1), @(d) d < 0);
-[E_ClassifyC, E_ClassifyD, E_ClassifyE] = get_error_cde(n_c, KNN(samples_c, samples_d, samples_e(:,1), samples_e(:,2), 1), KNN(samples_c, samples_e, samples_e(:,1), samples_e(:,2), 1), KNN(samples_d, samples_e, samples_e(:,1), samples_e(:,2), 1), @(d) d < 0);
+[C_ClassifyC, C_ClassifyD, C_ClassifyE] = get_error_cde(n_c, KNN(samples_c, samples_d, samples_c_2nd(:,1), samples_c_2nd(:,2), 1), KNN(samples_c, samples_e, samples_c_2nd(:,1), samples_c_2nd(:,2), 1), KNN(samples_d, samples_e, samples_c_2nd(:,1), samples_c_2nd(:,2), 1), @(d) d < 0);
+[D_ClassifyC, D_ClassifyD, D_ClassifyE] = get_error_cde(n_c, KNN(samples_c, samples_d, samples_d_2nd(:,1), samples_d_2nd(:,2), 1), KNN(samples_c, samples_e, samples_d_2nd(:,1), samples_d_2nd(:,2), 1), KNN(samples_d, samples_e, samples_d_2nd(:,1), samples_d_2nd(:,2), 1), @(d) d < 0);
+[E_ClassifyC, E_ClassifyD, E_ClassifyE] = get_error_cde(n_c, KNN(samples_c, samples_d, samples_e_2nd(:,1), samples_e_2nd(:,2), 1), KNN(samples_c, samples_e, samples_e_2nd(:,1), samples_e_2nd(:,2), 1), KNN(samples_d, samples_e, samples_e_2nd(:,1), samples_e_2nd(:,2), 1), @(d) d < 0);
 
 NN_conf_matrix_cde = [
     [C_ClassifyC, D_ClassifyC, E_ClassifyC];
