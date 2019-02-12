a2e9f61e098fcb1036521437ea3feabd80f23f24
Callum <mitccall@gmail.com>
2019-02-12 01:31:18 -0500

    Working confusion matrix for each data type (Note: for NN and KNN it may be useful to look into generating a new data set)

---
+++ lab1/scripts/get_error_KNN.m
@@ -0,0 +1,32 @@
+% Lab 1 KNN error 
+% Feb 12 2019
+% 
+% Kush Thaker - 20517901
+% Callum Mitchell - 20608435
+[A_ClassifyA, A_ClassifyB] = get_error(n_a, KNN(samples_a, samples_b, samples_a(:,1), samples_a(:,2), 5), @(d) d < 0);
+[B_ClassifyB, B_ClassifyA] = get_error(n_b, KNN(samples_a, samples_b, samples_b(:,1), samples_b(:,2), 5), @(d) d > 0);
+
+KNN_conf_matrix_ab = [
+    [A_ClassifyA, B_ClassifyA];
+    [A_ClassifyB, B_ClassifyB];
+];
+
+disp('Confusion matrix for KNN Case 1: A & B:');
+disp(KNN_conf_matrix_ab);
+
+P_error_KNN_case_1 = (B_ClassifyA + A_ClassifyB)/(n_a + n_b)
+
+[C_ClassifyC, C_ClassifyD, C_ClassifyE] = get_error_cde(n_c, KNN(samples_c, samples_d, samples_c(:,1), samples_c(:,2), 5), KNN(samples_c, samples_e, samples_c(:,1), samples_c(:,2), 5), KNN(samples_d, samples_e, samples_c(:,1), samples_c(:,2), 5), @(d) d < 0);
+[D_ClassifyC, D_ClassifyD, D_ClassifyE] = get_error_cde(n_c, KNN(samples_c, samples_d, samples_d(:,1), samples_d(:,2), 5), KNN(samples_c, samples_e, samples_d(:,1), samples_d(:,2), 5), KNN(samples_d, samples_e, samples_d(:,1), samples_d(:,2), 5), @(d) d < 0);
+[E_ClassifyC, E_ClassifyD, E_ClassifyE] = get_error_cde(n_c, KNN(samples_c, samples_d, samples_e(:,1), samples_e(:,2), 5), KNN(samples_c, samples_e, samples_e(:,1), samples_e(:,2), 5), KNN(samples_d, samples_e, samples_e(:,1), samples_e(:,2), 5), @(d) d < 0);
+
+KNN_conf_matrix_cde = [
+    [C_ClassifyC, D_ClassifyC, E_ClassifyC];
+    [C_ClassifyD, D_ClassifyD, E_ClassifyD];
+    [C_ClassifyE, D_ClassifyE, E_ClassifyE];
+];
+
+disp('Confusion matrix for KNN Case 2: C, D & E:');
+disp(KNN_conf_matrix_cde);
+
+P_error_KNN_case_2 = (D_ClassifyC + E_ClassifyC + C_ClassifyD + E_ClassifyD + C_ClassifyE + D_ClassifyE)/(n_c + n_d + n_e)
---
+++ lab1/scripts/get_error_MAP.m
@@ -0,0 +1,32 @@
+% Lab 1 MAP error 
+% Feb 12 2019
+% 
+% Kush Thaker - 20517901
+% Callum Mitchell - 20608435
+[A_ClassifyA, A_ClassifyB] = get_error(n_a, MAP(n_a, n_b, mean_a, mean_b, covar_a, covar_b, samples_a(:,1), samples_a(:,2)), @(d) d < 0);
+[B_ClassifyB, B_ClassifyA] = get_error(n_b, MAP(n_a, n_b, mean_a, mean_b, covar_a, covar_b, samples_b(:,1), samples_b(:,2)), @(d) d > 0);
+
+MED_conf_matrix_ab = [
+    [A_ClassifyA, B_ClassifyA];
+    [A_ClassifyB, B_ClassifyB];
+];
+
+disp('Confusion matrix for MED Case 1: A & B:');
+disp(MED_conf_matrix_ab);
+
+P_error_MED_case_1 = (B_ClassifyA + A_ClassifyB)/(n_a + n_b)
+
+[C_ClassifyC, C_ClassifyD, C_ClassifyE] = get_error_cde(n_c, MAP(n_c, n_d, mean_c, mean_d, covar_c, covar_d, samples_c(:,1), samples_c(:,2)), MAP(n_c, n_e, mean_c, mean_e, covar_c, covar_e, samples_c(:,1), samples_c(:,2)), MAP(n_d, n_e, mean_d, mean_e, covar_d, covar_e, samples_c(:,1), samples_c(:,2)), @(d) d < 0);
+[D_ClassifyC, D_ClassifyD, D_ClassifyE] = get_error_cde(n_c, MAP(n_c, n_d, mean_c, mean_d, covar_c, covar_d, samples_d(:,1), samples_d(:,2)), MAP(n_c, n_e, mean_c, mean_e, covar_c, covar_e, samples_d(:,1), samples_d(:,2)), MAP(n_d, n_e, mean_d, mean_e, covar_d, covar_e, samples_d(:,1), samples_d(:,2)), @(d) d < 0);
+[E_ClassifyC, E_ClassifyD, E_ClassifyE] = get_error_cde(n_c, MAP(n_c, n_d, mean_c, mean_d, covar_c, covar_d, samples_e(:,1), samples_e(:,2)), MAP(n_c, n_e, mean_c, mean_e, covar_c, covar_e, samples_e(:,1), samples_e(:,2)), MAP(n_d, n_e, mean_d, mean_e, covar_d, covar_e, samples_e(:,1), samples_e(:,2)), @(d) d < 0);
+
+MED_conf_matrix_cde = [
+    [C_ClassifyC, D_ClassifyC, E_ClassifyC];
+    [C_ClassifyD, D_ClassifyD, E_ClassifyD];
+    [C_ClassifyE, D_ClassifyE, E_ClassifyE];
+];
+
+disp('Confusion matrix for MED Case 2: C, D & E:');
+disp(MED_conf_matrix_cde);
+
+P_error_MED_case_2 = (D_ClassifyC + E_ClassifyC + C_ClassifyD + E_ClassifyD + C_ClassifyE + D_ClassifyE)/(n_c + n_d + n_e)
