283d951f26e533638d00e71e2c8470d3f4234223
Callum <mitccall@gmail.com>
2019-03-31 18:37:42 -0400

    2D estimation code tweaking and cleanup | added 2D scatterplot and ML boundary figure

--- lab2/estimate_2d.m
+++ lab2/estimate_2d.m
@@ -11,8 +11,8 @@ load('lab2_2.mat')
 %% Parzen window normal func 
 
 % Window is 2D gaussian with variance of 400 on both axes
-x_pzn = -600:1:600;
-y_pzn = -600:1:600;
+x_pzn = -500:1:500;
+y_pzn = -500:1:500;
 mean_pzn = [0, 0];
 covar_pzn = [400,0 ; 0,400];
 
@@ -25,80 +25,45 @@ win_pzn = reshape(win_pzn, length(x_pzn), length(y_pzn));
 % parametric, assume normal
 
 % for class a, plot p_est (norm) and true p (norm)
-x_a = -100:1:500;
-y_a = -100:1:500;
-[mean_e, covar_e] = ml_normal_2d(al); 
+x1_range =  0:1:500;
+x2_range =  0:1:500;
+[mean_al, covar_al] = ml_normal_2d(al); 
 
-[X1,X2] = meshgrid(x_a, y_a);
+[X1,X2] = meshgrid(x1_range, x2_range);
 X = [X1(:) X2(:)];
-p_hat_a_gauss = mvnpdf(X, mean_e,covar_e);
-p_hat_a_gauss = reshape(p_hat_a_gauss, length(x_a), length(y_a));
+p_hat_a_gauss = mvnpdf(X, mean_al, covar_al);
+p_hat_a_gauss = reshape(p_hat_a_gauss, length(x1_range), length(x2_range));
 
 % for class b, plot p_est (norm) and true p (exp)
-x_b = -100:1:500;
-y_b = -100:1:500;
-[mean_e, covar_e] = ml_normal_2d(bl);
+[mean_bl, covar_bl] = ml_normal_2d(bl);
 
-[X1,X2] = meshgrid(x_b, y_b);
+[X1,X2] = meshgrid(x1_range, x2_range);
 X = [X1(:) X2(:)];
-p_hat_b_gauss = mvnpdf(X, mean_e,covar_e);
-p_hat_b_gauss = reshape(p_hat_b_gauss, length(x_b), length(y_b));
+p_hat_b_gauss = mvnpdf(X, mean_bl, covar_bl);
+p_hat_b_gauss = reshape(p_hat_b_gauss, length(x1_range), length(x2_range));
 
 % for class c, plot p_est (norm) and true p (exp)
-x_c = -100:1:500;
-y_c = -100:1:500;
-[mean_e, covar_e] = ml_normal_2d(cl);
+[mean_cl, covar_cl] = ml_normal_2d(cl);
 
-[X1,X2] = meshgrid(x_c, y_c);
+[X1,X2] = meshgrid(x1_range, x2_range);
 X = [X1(:) X2(:)];
 
-p_hat_c_gauss = mvnpdf(X, mean_e,covar_e);
-p_hat_c_gauss = reshape(p_hat_c_gauss, length(x_c), length(y_c));
+p_hat_c_gauss = mvnpdf(X, mean_cl, covar_cl);
+p_hat_c_gauss = reshape(p_hat_c_gauss, length(x1_range), length(x2_range));
 
 
 %% Non-parametric Estimate
 
 % for class a, plot p_hat (std 20) and true p (norm)
-[p_hat_a_parzen, x, y] = parzen_2d(al, [1, -100, -100, 499, 499], win_pzn);
-data = zeros(2, max(length(x), length(y)));
-for i=1:length(x)
-    data(1,i) = x(i);
-end
-for i=1:length(y)
-    data(2,i) = y(i);
-end
+[p_hat_a_parzen, x, y] = parzen_2d(al, [1, 0, 0, 500, 500], win_pzn);
 
 % for class b, plot p_hat (std 20) and true p (norm)
-[p_hat_b_parzen, x, y] = parzen_2d(bl, [1, -100, -100, 499, 499], win_pzn);
-data = zeros(2, max(length(x), length(y)));
-for i=1:length(x)
-    data(1,i) = x(i);
-end
-for i=1:length(y)
-    data(2,i) = y(i);
-end
+[p_hat_b_parzen, x, y] = parzen_2d(bl, [1, 0, 0, 500, 500], win_pzn);
 
 % for class c, plot p_hat (std 20) and true p (norm)
-[p_hat_c_parzen, x, y] = parzen_2d(cl, [1, -100, -100, 499, 499], win_pzn);
-data = zeros(2, max(length(x), length(y)));
-for i=1:length(x)
-    data(1,i) = x(i);
-end
-for i=1:length(y)
-    data(2,i) = y(i);
-end
+[p_hat_c_parzen, x, y] = parzen_2d(cl, [1, 0, 0, 500, 500], win_pzn);
 
 %% ML decision boundary and data scatter plots
-
-LINE_WIDTH = 2;
-
-figure(1);
-hold on; 
-
-% Plot testing sample data
-scatter_a = scatter(at(:,1), at(:,2), 'k^', 'filled');
-scatter_b = scatter(bt(:,1), bt(:,2), 'bx');
-scatter_c = scatter(ct(:,1), ct(:,2), 'ro', 'filled');
 
 %calculate ML boundary based on discretized Gaussian and Parzen pdf values
 %of each class
@@ -113,10 +78,7 @@ for i=1:size(X1,1)
             ML_Gauss(i,j) = 1;
         else ML_Gauss(i,j) = 2;
         end
-    end
-end
-for i=1:size(X1,1)
-    for j=1:size(X2,1)
+        
         %Parzen window estimation
         if p_hat_a_parzen(i,j) > p_hat_b_parzen(i,j) && p_hat_a_parzen(i,j) > p_hat_c_parzen(i,j)
             ML_Parzen(i,j) = -1;
@@ -129,6 +91,22 @@ for i=1:size(X1,1)
     end
 end
 
+LINE_WIDTH = 2;
+
+figure(1);
+hold on; 
+
+title('2D Class Samples, ML Gaussian and ML Parzen decision boundaries');
+xlabel('x1');
+ylabel('x2');
+
+% Plot testing sample data
+scatter_a = scatter(at(:,1), at(:,2), 'k^', 'filled');
+scatter_b = scatter(bt(:,1), bt(:,2), 'bx');
+scatter_c = scatter(ct(:,1), ct(:,2), 'ro', 'filled');
+
 % Plot ML boundary
 [~,ML_gauss_boundary] = contour(X1,X2,ML_Gauss, 'Color', 'r', 'LineWidth', LINE_WIDTH);
 [~,ML_parzen_boundary] = contour(X1,X2,ML_Parzen, 'Color', 'b', 'LineWidth', LINE_WIDTH);
+
+legend([scatter_a, scatter_b, scatter_c, ML_gauss_boundary, ML_parzen_boundary], {'Class A', 'Class B', 'Class C', 'Gaussian ML Decision Boundary', 'Parzen ML Decision Boundary'});
